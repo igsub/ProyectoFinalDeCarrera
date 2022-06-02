d11230581dbaca9fa3990b5e6ecab9bc1cb558de
igsub <igsub@hotmail.com>
2022-06-02 18:05:33 -0300

    archivos y cambios para produccion

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,8 +2,10 @@ import axios from "axios"
 
 import userService from "./Services/userService"
 
+const EXPRESS_HOST = process.env.NODE_ENV === "production" ? "https://meet-app.duckdns.org/api" : "http://localhost:5000";
+
 const instance = axios.create({
-	baseURL: "http://localhost:5000",
+	baseURL: `${EXPRESS_HOST}`,
 })
 
 instance.interceptors.request.use(
--- client/src/services/weatherService.js
+++ client/src/services/weatherService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const weatherService = {
 	getWeatherByDate: async (coords) => {
-		return axios.get(`http://api.openweathermap.org/data/2.5/forecast?lat=${coords.lat}&lon=${coords.lon}&lang=es&units=metric&appid=${process.env.REACT_APP_WHEATHER_API_KEY}`)
+		return axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${coords.lat}&lon=${coords.lon}&units=metric&appid=${process.env.REACT_APP_WHEATHER_API_KEY}`)
 	},
 }
 
